95aa48774be0538eacceeafefc233a63beefce47
hsn <systemry420gmail.com>
2020-10-29 14:20:18 +0200

    control the text input

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    value: ''
+  }
 
+  handleChange = e => {
+    this.setState({
+      value: e.target.value
+    })
+  }
 
   render(){
     return (
       <div className="App">
         <h1>Weight Converter</h1>
-        <input type="text" />
+        <input
+          type="text" 
+          onChange={this.handleChange}
+          value={this.state.value}
+        />
       </div>
     )
   }
